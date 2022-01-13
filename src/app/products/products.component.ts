8feeedbc7d335013c67b3c8c42148382ead9e8db
nikithay0200 <nikithay0200@gmail.com>
2022-01-13 16:37:28 +0530

    root and admin modules

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product.model';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent  {
+
+
+//   products:Product[]=[
+//   {
+//     productTitle:"Technology-1",
+//     description:"This is the top most technology in today's world. So let's learn this now.",
+//     productImage:"https://wallpaperaccess.com/full/4755970.jpg"
+//   },
+//   {
+//     productTitle:"Technology-2",
+//     description:"This is the top most technology in today's world. So let's learn this now.",
+//     productImage:"https://wallpaperaccess.com/full/4755970.jpg"
+//   },
+//   {
+//     productTitle:"Technology-3",
+//     description:"This is the top most technology in today's world. So let's learn this now.",
+//     productImage:"https://wallpaperaccess.com/full/4755970.jpg"
+//   },
+//   {
+//     productTitle:"Technology-4",
+//     description:"This is the top most technology in today's world. So let's learn this now.",
+//     productImage:"https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fdam%2Fimageserve%2F966248982%2F960x0.jpg%3Ffit%3Dscale"
+//   },
+//   {
+//     productTitle:"Technology-5",
+//     description:"This is the top most technology in today's world. So let's learn this now.",
+//     productImage:"https://wallpaperaccess.com/full/4755970.jpg"
+//   },
+//   {
+//     productTitle:"Technology-6",
+//     description:"This is the top most technology in today's world. So let's learn this now.",
+//     productImage:"https://wallpaperaccess.com/full/4755970.jpg"
+//   },
+//   {
+//     productTitle:"Technology-7",
+//     description:"This is the top most technology in today's world. So let's learn this now.",
+//     productImage:"https://wallpaperaccess.com/full/4755970.jpg"
+//   },
+//   {
+//     productTitle:"Technology-8",
+//     description:"This is the top most technology in today's world. So let's learn this now.",
+//     productImage:"https://wallpaperaccess.com/full/4755970.jpg"
+//   }
+// ];
+
+// productsSentbyChild=[];
+// productCount:number=0;
+// getProductDetailsfromChild(productTitle){
+//     this.productsSentbyChild.push(productTitle);
+//     this.productCount++;
+// }
+
+}
